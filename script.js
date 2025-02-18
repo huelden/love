bcce85c7c53ea363106be572294c4ad45d21854f
huelden <156624639+huelden@users.noreply.github.com>
2025-02-18 12:33:41 +0000

    finalizado

---
+++ script.js
@@ -0,0 +1,22 @@
+document.getElementById('login').addEventListener('submit',function(event) {
+    event.preventDefault();
+
+    const username=
+
+    document.getElementById('username').value;
+
+    const password=
+
+    document.getElementById('password').value;
+
+    //Aqui voce pode definir um usuario e senhe especificos 
+
+    if(username==='lindos' && password==='olhos' ) {
+        document.getElementById('login-form').classList.add('hidden');
+
+        document.getElementById('surpresa').classList.remove('hidden');
+    } else {
+        alert('Usuário ou senha incorretos!');
+    }
+
+});
